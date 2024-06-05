3949059eb9e3d7dbaf9264fb0a51a6d40ecbb7a9
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-06 06:40:19 +0900

    set app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import LandingPage from "./Components/LandingPage/LandingPage";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+import Register from "./Components/Register/Register";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <Home/>
+
+  );
+}
+
+export default App;
