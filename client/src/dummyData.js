fb7c162b483ba43f9f8f2e4e35539da5a722f1d4
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-06 06:45:11 +0900

    Create DummyData

---
+++ client/src/dummyData.js
@@ -0,0 +1,154 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "assets/1.jpg",
+        username: "Safak Kocaoglu",
+    },
+    {
+        id: 2,
+        profilePicture: "assets/2.jpg",
+        username: "Janeli Shrum",
+    },
+    {
+        id: 3,
+        profilePicture: "assets/3.jpg",
+        username: "Aiexa Durden",
+    },
+    {
+        id: 4,
+        profilePicture: "assets/4.jpg",
+        username: "Dora Hawks",
+    },
+    {
+        id: 5,
+        profilePicture: "assets/5.jpg",
+        username: "Thomas Holden",
+    },
+    {
+        id: 6,
+        profilePicture: "assets/6.jpeg",
+        username: "Shirleya Beauchamo",
+    },
+    {
+        id: 7,
+        profilePicture: "assets/7.jpg",
+        username: "Travisiya Bennety",
+    },
+    {
+        id: 8,
+        profilePicture: "assets/8.jpg",
+        username: "Kristena THomas",
+    },
+    {
+        id: 9,
+        profilePicture: "assets/9.jpg",
+        username: "Lauriya Botheju",
+    },
+    {
+        id: 10,
+        profilePicture: "assets/10.jpg",
+        username: "Gary Duty",
+    },
+
+    ];
+
+    export const Posts = [
+    {
+        id: 1,
+        desc: "Love For All, Hatred For None.",
+        photo: "assets/post1.jpg",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,
+    },
+
+    {
+        id: 2,
+        desc: "Sweet Home.",
+        photo: "assets/post11.jpeg",
+        date: "15 mins ago",
+        userId: 2,
+        like: 27,
+        comment: 15,
+    },
+
+    {
+        id: 3,
+        desc: "Love Nature.",
+        photo: "assets/post13.jpeg",
+        date: "10 mins ago",
+        userId: 3,
+        like: 20,
+        comment: 12,
+    },
+
+    {
+        id: 4,
+        photo: "assets/cover2.jpg",
+        date: "1 hour ago",
+        userId: 4,
+        like: 25,
+        comment: 10,
+    },
+
+    {
+        id: 5,
+        desc: "A Night",
+        photo: "assets/post14.jpeg",
+        date: "30 mins ago",
+        userId: 5,
+        like: 30,
+        comment: 18,
+    },
+
+    {
+        id: 6,
+        desc: "Morning vibes.",
+        photo: "assets/post15.jpeg",
+        date: "10 mins ago",
+        userId: 6,
+        like: 20,
+        comment: 12,
+    }, 
+
+    {
+        id: 7,
+        desc: "Love Nature.",
+        photo: "assets/post16.jpeg",
+        date: "1 day ago",
+        userId: 7,
+        like: 29,
+        comment: 20,
+    }, 
+
+    {
+        id: 8,
+        photo: "assets/post17.jpeg",
+        date: "20 mins ago",
+        userId: 8,
+        like: 10,
+        comment: 5,
+    },
+
+    {
+        id: 9,
+        desc: "Working Time",
+        photo: "assets/post20.webp",
+        date: "45 mins ago",
+        userId: 9,
+        like: 27,
+        comment: 12,
+    },
+
+    {
+        id: 10,
+        photo: "assets/post19.jpeg",
+        date: "1 mins ago",
+        userId: 10,
+        like: 8,
+        comment: 2,
+    }
+        
+    ]
+
