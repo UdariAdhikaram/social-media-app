b8a14b7a1b192dca009730e50a9e740315d0db0b
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-06 06:43:02 +0900

    create Register

---
+++ client/src/Components/Register/Register.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+
+import "./register.css"
+
+export default function Register() {
+
+    return (
+        <div className="register">
+            <div className="registerWrapper">
+                <div className="registerLeft">
+                    <h3 className="registerLogo">StoryBook</h3>
+                    <span className="registerDesc">
+                    Connect with friends and 
+                    the world around you on StoryBook.
+                </span>
+                </div>
+                <div className="registerRight">
+                    <form className="registerBox">
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            className="registerInput"
+                            
+                        />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            className="registerInput"
+                            
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className="registerInput"
+                            
+                        />
+                        <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            className="registerInput"
+                        />
+                        <div className="nameContainer">
+                            <div className="fnameLeft">
+                                <input
+                                    placeholder="First name"
+                                    className="registerInput"
+                                    
+                                />
+                            </div>
+                            <div className="lnameRight">
+                                <input
+                                    placeholder="Last name"
+                                    className="registerInput"
+                                    
+                                />
+                            </div>
+                        </div>
+                        <input
+                            type="text"
+                            placeholder="Date of Birth"
+                        />
+                        <button className="registerButton">Sign Up</button>
+                        <br/>
+                        Already have an account ?
+                        <button className="logRegisterButton">Log in</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
