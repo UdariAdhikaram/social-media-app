03a0b74c652545aca63ca5a40eeb1eb27823757f
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-07 04:07:02 +0900

    update Feed.jsx

--- client/src/Components/feed/Feed.jsx
+++ client/src/Components/feed/Feed.jsx
@@ -2,6 +2,7 @@ import Post from "../post/Post"
 import Share from "../share/Share"
 import "./feed.css"
 import {Posts} from "../../dummyData"
+
 export default function Feed() {
   return (
     <div className="feed">
