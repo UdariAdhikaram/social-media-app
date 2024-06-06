68988abc314751252228b6ab895d196751ad3a64
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-07 04:06:47 +0900

    remove some import files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,3 @@
-import LandingPage from "./Components/LandingPage/LandingPage";
-import LoginSignup from "./Components/LoginSignup/LoginSignup";
-import Register from "./Components/Register/Register";
 import Home from "./pages/home/Home";
 
 function App() {
