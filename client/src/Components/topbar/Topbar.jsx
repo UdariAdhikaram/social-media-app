f5d8390cfdd219e29cdd7f57fd4f4bab698e36aa
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-08 01:56:35 +0900

    add new items to topbar

--- client/src/Components/topbar/Topbar.jsx
+++ client/src/Components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.css"
+import { Search, Person, Chat, Notifications, Logout } from "@mui/icons-material"
 
 export default function Topbar() {
   return (
@@ -6,11 +7,31 @@ export default function Topbar() {
         <div className="topbarLeft">
             <span className="logo">StoryBook</span>
         </div>
-
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon"/>
+            <input placeholder="Search for friend, post or video" className="searchInput" />
+          </div>
+        </div>
         <div className="topbarRight">
             <div className="topbarLinks">
                 <span className="topbarLink">Homepage</span>
-                <span className="topbarLink">Logout</span>
+                <span className="topbarLink">Tiimeline</span>
+                
+            </div>
+            <div className="topbarIcons">
+              <div className="topbarIconItem">
+                <Person/>
+                <span className="topbarIconBadge">1</span>
+              </div>
+              <div className="topbarIconItem">
+                <Chat/>
+                <span className="topbarIconBadge">2</span>
+              </div>
+              <div className="topbarIconItem">
+                <Notifications/>
+                <span className="topbarIconBadge">1</span>
+              </div>
             </div>
             <img src="/assets/4.jpg" alt="" className="topbarImg"/>
         </div>
