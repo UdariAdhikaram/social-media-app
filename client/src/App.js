d8b807878ec53963a42fd0f3d763f33b85d0c41a
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-08 01:50:30 +0900

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+import Register from "./Components/Register/Register";
+import Login from "./Components/login/Login";
+import Profile from "./Components/profile/Profile";
 import Home from "./pages/home/Home";
 
 function App() {
