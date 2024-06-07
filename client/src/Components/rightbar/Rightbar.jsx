333f3f9556a32c732e45c8145050955a616ab025
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-08 01:54:59 +0900

    update profileRightbar

--- client/src/Components/rightbar/Rightbar.jsx
+++ client/src/Components/rightbar/Rightbar.jsx
@@ -2,11 +2,11 @@ import "./rightbar.css"
 import {Users} from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Rightbar() {
+export default function Rightbar({profile}) {
+
+  const HomeRightbar = () => {
     return(
       <>
-      <div className="rightbar">
-      <div className="rightbarWrapper">
       <div className="birthdayContainer">
             <img src="assets/gift.jpg" alt="" className="birthdayImg"/>
             <span className="birthdayText">
@@ -20,8 +20,80 @@ export default function Rightbar() {
             <Online key={u.id} user={u}/>
           ))}
           </ul>
-          </div>
-        </div>
       </>
     );
   };
+
+  const ProfileRightbar = () =>{ 
+    return(
+        <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="profileInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York:</span>
+          </div>
+          <div className="profileInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Madrid:</span>
+          </div>
+          <div className="profileInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single:</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollwings">
+          <div className="rightbarFollowing">
+            <img src="assets/5.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/7.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/9.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/10.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/CurentProfile.jpeg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/feed11.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/8.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/feed6.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/story4.jpg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="assets/feed5.jpeg" alt="" className="rightFollowingImg"/>
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
+        </>
+    );
+  }
+
+  return (
+    <div className="rightbar">
+        <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar/> : <HomeRightbar/>}
+        </div>
+    </div>
+  )
+}
