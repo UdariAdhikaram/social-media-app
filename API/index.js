f5e6646da901eeb4a7e88ddd5e57767c5b6f980d
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-11 03:06:00 +0900

    connect mongoose and routes users.js and auth.js

--- API/index.js
+++ API/index.js
@@ -4,13 +4,34 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true},()=>{
-    console.log("Connected to MongoDb")
+mongoose.connect(process.env.MONGO_URL, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true })
+  
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Could not connect to MongoDB", err));
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+/*app.get("/",(req,res)=>{
+  res.send("welcome to home page")
 });
 
-app.listen(8800,()=>{
+app.get("/users",(req,res)=>{
+  res.send("welcome to user page")
+});*/
+
+app.listen(8000,()=>{
     console.log("Backend server is running!..")
-})
+});
