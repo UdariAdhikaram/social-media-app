6ccb110fd39960172ceb181c7325b3e17c04986c
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-12 04:55:30 +0900

    set timeline posts

--- API/routes/post.js
+++ API/routes/post.js
@@ -69,5 +69,20 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+// Get timeline posts
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId); // Use query parameters instead
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendID) => {
+                return Post.find({ userId: friendID });
+            })
+        );
+        res.status(200).json(userPosts.concat(...friendPosts)); // Send response with status 200
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
