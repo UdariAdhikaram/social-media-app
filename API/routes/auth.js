cc6d3af097fea17120895a5ae39b7512858d1b50
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-11 21:08:06 +0900

    manage error message

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -23,7 +23,7 @@ router.post("/register", async (req,res)=>{
         
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).json(error);
     }
 });
 
@@ -41,8 +41,7 @@ router.post("/login", async (req,res)=> {
         res.status(200).json(user);
 
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).json(error);
     }
     
 })
