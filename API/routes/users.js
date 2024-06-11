da18fb8d90257d9ba181be2c8cb3af6c3c349356
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-11 23:31:01 +0900

    set get a user

--- API/routes/users.js
+++ API/routes/users.js
@@ -46,6 +46,16 @@ router.delete("/:id", async (req,res) =>{
 });
 
 //get a user
+router.get("/:id", async (req,res) =>{
+        try{
+            const user = await User.findById(req.params.id);
+            const {password,updatedAt, ...other} = user._doc
+            res.status(200).json(other)
+        }catch(error){
+            return res.status(500).json(error);
+        }
+});
+
 //follow a user
 router.put("/:id/follow", async (req,res) =>{
     if(req.body.userId !== req.params.id){ //check users are same or not
