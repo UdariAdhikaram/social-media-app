27699f69a5335c63370702d8d67ac066c037f17d
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-12 04:54:27 +0900

    update post set

--- API/routes/post.js
+++ API/routes/post.js
@@ -13,6 +13,21 @@ router.post("/", async (req, res) => {
     }
 });
 
+// Update a post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("The post has been updated");
+        } else {
+            res.status(403).json("You can update only your post");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 
 module.exports = router;
