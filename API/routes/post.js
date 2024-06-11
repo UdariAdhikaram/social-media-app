2c8a329f56a585f7347e772d05f98df8e24a0656
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-12 04:53:46 +0900

    create post

--- API/routes/post.js
+++ API/routes/post.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 
 router.get("/",(req,res)=>{
     console.log("post");
+// Create a post
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 
