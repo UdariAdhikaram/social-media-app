5b409f2d85935313bb551b2f8f644c0b5cffcf87
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-13 19:59:41 +0900

    create route

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,30 @@ import Register from "./Components/Register/Register";
 import Login from "./Components/login/Login";
 import Profile from "./Components/profile/Profile";
 import Home from "./pages/home/Home";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/profile/:username">
+          <Profile/>
+        </Route>
+      </Switch>
+    </Router>
 
   );
 }
