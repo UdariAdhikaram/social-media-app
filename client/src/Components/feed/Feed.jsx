209eb640063e1ef8ce56dfad24f43629488e5ec8
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-15 04:30:53 +0900

    update

--- client/src/Components/feed/Feed.jsx
+++ client/src/Components/feed/Feed.jsx
@@ -1,19 +1,34 @@
-import Post from "../post/Post"
-import Share from "../share/Share"
-import "./feed.css"
-import {Posts} from "../../dummyData"
+import { useEffect, useState } from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import axios from "axios";
 
-export default function Feed() {
+export default function Feed({ username }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = username
+          ? await axios.get("/posts/profile/" + username)
+          : await axios.get("/posts/timeline/666c5057969a79f83d730852");
+        setPosts(res.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, [username]);
+
   return (
     <div className="feed">
-        <div className="feedWrapper">
-          <Share/>
-          {Posts.map((p) => (
-            <Post key={p.id} post={p}/>
-          ))}
-          
-          
-        </div>
+      <div className="feedWrapper">
+        <Share />
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
