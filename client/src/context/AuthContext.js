026c7359c646f8a5f65c9a1ca5ce0fcd50265e77
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-16 23:54:29 +0900

    create AuthContext

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,25 @@
+const INITIAL_STATE = {
+    user: null,
+    isFecthing: false,
+    error:false,
+};
+
+export const AuthContext = createContext(INITIAL_STATE);
+
+export const AuthContextProvider = ({children}) =>{
+    const [state, dispatch] = useReducer(AuthContext, INITIAL_STATE);
+
+    return(
+        <AuthContext.Provider 
+        value ={{
+            user: state.user,
+            isFecthing: state.isFecthing,
+            error: state.error,
+            dispatch
+        }}
+        >
+        {children}
+        </AuthContext.Provider>
+
+    );
+};
