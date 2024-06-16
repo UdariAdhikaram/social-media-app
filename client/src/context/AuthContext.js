134c8ceeb19c4b0c43e61a4500f86f2d593e39db
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-17 05:58:50 +0900

    Create AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,19 +1,31 @@
+import { createContext, useReducer } from "react";
+import AuthReducer from "./AuthReducer";
+
 const INITIAL_STATE = {
-    user: null,
-    isFecthing: false,
-    error:false,
+    user:{
+    _id: "666c5057969a79f83d730852",
+    username:"john",
+    email:"john@gmail.com",
+    password:"$2b$10$0B9T2qLoIQElJ.c8yUReVuc0PnXeiWOLYqCDJ4z6E8x0Pk6.67eve",
+    profilePicture:"1.jpg",
+    coverPicture:"",
+    followers: [],
+    followings: [],
+    },
+    isFetching: false,
+    error: false,
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({children}) =>{
-    const [state, dispatch] = useReducer(AuthContext, INITIAL_STATE);
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
     return(
         <AuthContext.Provider 
         value ={{
             user: state.user,
-            isFecthing: state.isFecthing,
+            isFetching: state.isFetching,
             error: state.error,
             dispatch
         }}
