a4042b2de3bc1c1718b4f79b30c7bbb1fc4dcdb0
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-17 05:55:02 +0900

    set AuthContext.js for  connect API

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
+import { useContext } from "react";
 import Register from "./Components/Register/Register";
 import Login from "./Components/login/Login";
 import Profile from "./Components/profile/Profile";
 import Home from "./pages/home/Home";
-import{
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import{BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
+
+  const {user} = useContext(AuthContext)
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home/>
+          { user ? <Home /> : <Register/>}
         </Route>
         <Route path="/login">
-          <Login/>
+          {user ? <Redirect to="/" /> : <Login />} 
         </Route>
         <Route path="/register">
-          <Register/>
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
         <Route path="/profile/:username">
           <Profile/>
