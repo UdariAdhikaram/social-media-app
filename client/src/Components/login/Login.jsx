0102d771a3c5bacb9263ba0457ca2a34397f20ef
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-16 23:52:42 +0900

    set login varification

--- client/src/Components/login/Login.jsx
+++ client/src/Components/login/Login.jsx
@@ -1,41 +1,39 @@
-import React, {useContext, useRef} from 'react';
-import {useHistory} from 'react-router-dom';
-
+import React, {useRef} from 'react';
 import "./login.css"
 
 export default function Login() {
-   
-
+const email = useRef();
+const password = useRef();
+const handleClick = (e) =>{
+    e.preventDefault();
+    console.log(email);
+}
     return (
         <div className="login">
             <div className="loginWrapper">
                 <div className="loginLeft">
                     <h3 className="loginLogo">StoryBook</h3>
                     <span className="loginDesc">
-                    Connect with friends and
-                    the world around you on StoryBook.
+                    Connect with friends and the world around you on StoryBook.
                 </span>
                 </div>
 
                 <div className="loginRight">
-                    <div className="loginBox">
+                    <form className="loginBox" onSubmit={handleClick}>
                         <input
-                            type="text"
+                            type="email"
                             //ref={emailRef}
-                            placeholder="Username"
-                            className="loginInput"
+                            placeholder="Username" required className="loginInput" ref={email}
                         />
                         <input
                             type="password"
                             //ref={passwordRef}
-                            placeholder="Password"
-                            className="loginInput"
-                        />
+                            placeholder="Password" required minLength="6" className="loginInput" ref={password}/>
                         <button className="loginButton">Log In</button>
                         <span className="loginForgot">Forgot Password?</span>
                         <span className="LoginRegister">Don't have an account, Click here to</span>
                         <button className="loginRegisterButton">Sign Up</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
