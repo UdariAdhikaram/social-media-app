3ebc6b58a9cb929df46446b4da8687af80b47ea5
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-18 05:34:59 +0900

    post request with axios

--- client/src/Components/post/Post.jsx
+++ client/src/Components/post/Post.jsx
@@ -1,15 +1,21 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { format} from 'timeago.js';
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Post({ post }) {
     const [like,setLike] = useState(post.likes.length);
     const [isliked,setIsLiked] = useState(false);
     const [user,setUser] = useState({});
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const {user:currentUser} = useContext(AuthContext);
+
+    useEffect(()=>{
+        setIsLiked(post.likes.includes(currentUser._id))
+    },[currentUser._id,post.likes]);
 
     useEffect(()=>{
         const fetchUser = async () => {
@@ -20,6 +26,11 @@ export default function Post({ post }) {
       },[post.userId]);
 
     const likeHandler =()=>{
+        try {
+            axios.put("/posts/"+post._id+"/like", {userId:currentUser._id})
+        } catch (error) {
+            
+        }
         setLike(isliked ? like-1 : like+1)
         setIsLiked(!isliked)
     }
@@ -32,7 +43,7 @@ export default function Post({ post }) {
 
                     <Link to={`profile/${user.username}`}>
                     <img className="postProfileImg" 
-                    src={user.profilePicture || PF+"noAvatar.jpg"} alt=""/>
+                    src={user.profilePicture ? PF + user.profilePicture : PF+"noAvatar.jpg"} alt=""/>
                     </Link>
 
                     <span className="postUsername">
